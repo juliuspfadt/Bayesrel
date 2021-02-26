26dcc3367417d4ea70c2c4ffb650d496468bdcd8
Julius Pfadt <julius.pfadt@gmail.com>
2021-02-26 12:40:25 +0100

    fix bug with single cov matrix and empty function call for glb in Cpp

--- R/glb_funs.R
+++ R/glb_funs.R
@@ -61,12 +61,7 @@ glbOnArray_custom <- function(Cov, callback = function(){}, printlevel = 0) {
   )
   return(ret)
 
-  scv <- apply(Cov, 1, sum)
-  vars <- apply(Cov, 1, diag)
-  svars <- apply(vars, 2, sum)
-  glbs <- (scv - svars + ret) / scv
 
-  return(glbs)
 }
 
 get.prob.info2 <- function(K, m) {
