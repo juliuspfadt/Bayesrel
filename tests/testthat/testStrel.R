dad83e0ab8405f52111176e62d75a392b36dcd3d
Julius Pfadt <julius.pfadt@gmail.com>
2022-12-07 12:08:09 +0100

    fix tests with disable MCMC check

--- tests/testthat/testStrel.R
+++ tests/testthat/testStrel.R
@@ -5,7 +5,8 @@ test_that("Estimates lambda2 and omega are correct", {
 
   data(asrm, package = "Bayesrel")
   set.seed(1234)
-  ee <- Bayesrel::strel(asrm, estimates = c("lambda2", "omega"), n.iter = 100, n.boot = 100, n.chains = 2)
+  ee <- Bayesrel::strel(asrm, estimates = c("lambda2", "omega"), n.iter = 100, n.boot = 100, n.chains = 2,
+                        disableMCMCCheck = TRUE)
 
   expect_equal(ee$Bayes$est$Bayes_lambda2, 0.7948049, tolerance = tol)
   expect_equal(ee$freq$est$freq_lambda2, 0.7960336, tolerance = tol)
@@ -23,7 +24,8 @@ test_that("Bayes glb is correct", {
 
   data(asrm, package = "Bayesrel")
   set.seed(1234)
-  ee <- Bayesrel::strel(asrm, estimates = "glb", n.iter = 100, freq = F, n.chains = 1)
+  ee <- Bayesrel::strel(asrm, estimates = "glb", n.iter = 100, freq = FALSE, n.chains = 1,
+                        disableMCMCCheck = TRUE)
 
   expect_equal(ee$Bayes$est$Bayes_glb, 0.8542316, tolerance = tol)
   expect_equal(ee$Bayes$cred$up$Bayes_glb, 0.8950283, tolerance = tol)
@@ -36,7 +38,8 @@ test_that("Bayes alpha and omega if item deleted is correct", {
 
   data(asrm, package = "Bayesrel")
   set.seed(1234)
-  ee <- Bayesrel::strel(asrm, estimates = c("alpha", "omega"), n.iter = 100, freq = F, item.dropped = T, n.chains = 2)
+  ee <- Bayesrel::strel(asrm, estimates = c("alpha", "omega"), n.iter = 100, freq = FALSE, item.dropped = TRUE,
+                        n.chains = 2, disableMCMCCheck = TRUE)
   expect_equal(as.numeric(ee$Bayes$ifitem$est$alpha[1:2]), c(0.7207363, 0.7245768),
                tolerance = tol)
   expect_equal(as.numeric(ee$Bayes$ifitem$cred$alpha[c(1, 10)]), c(0.6450673, 0.8049170),
@@ -53,7 +56,7 @@ test_that("Freq omega with PFA is correct", {
 
   data(asrm, package = "Bayesrel")
   set.seed(1234)
-  tt <- Bayesrel::strel(asrm, estimates = "omega", n.boot = 100, Bayes = F, omega.freq.method = "pfa")
+  tt <- Bayesrel::strel(asrm, estimates = "omega", n.boot = 100, Bayes = FALSE, omega.freq.method = "pfa")
   expect_equal(as.numeric(tt$freq$est$freq_omega), c(0.7966209), tolerance = tol)
   if (as.numeric(R.Version()$major >= 4)) {
     expect_equal(as.numeric(tt$freq$conf$up$freq_omega), 0.8617495, tolerance = tol)
@@ -66,7 +69,8 @@ test_that("Bayes prior and posterior probability for Alpha >.8 is correct", {
 
   data(asrm, package = "Bayesrel")
   set.seed(1234)
-  ee <- Bayesrel::strel(asrm, estimates = "alpha", n.iter = 100, freq = F, n.chains = 2)
+  ee <- Bayesrel::strel(asrm, estimates = "alpha", n.iter = 100, freq = FALSE, n.chains = 2,
+                        disableMCMCCheck = TRUE)
   tt <- Bayesrel::pStrel(ee, "alpha", .8)
 
   expect_equal(as.numeric(tt), c(0.1556125, 0.3300000), tolerance = tol)
@@ -78,7 +82,8 @@ test_that("Omega results with missing data are correct", {
 
   data(asrm_mis, package = "Bayesrel")
   set.seed(1234)
-  ee <- Bayesrel::strel(asrm_mis, estimates = c("omega"), n.iter = 100, n.chains = 2, n.boot = 100)
+  ee <- Bayesrel::strel(asrm_mis, estimates = c("omega"), n.iter = 100, n.chains = 2, n.boot = 100,
+                        disableMCMCCheck = TRUE)
   expect_equal(as.numeric(ee$Bayes$cred$low$Bayes_omega), c(0.6900274),
                tolerance = tol)
   expect_equal(as.numeric(ee$freq$est$freq_omega), c(0.7943602),
@@ -90,7 +95,7 @@ test_that("Frequentist Lambda6 results with missing data and parametric bootstra
 
   data(asrm_mis, package = "Bayesrel")
   set.seed(1234)
-  ee <- Bayesrel::strel(asrm_mis, estimates = c("lambda6"), Bayes = F, n.boot = 100, para.boot = T)
+  ee <- Bayesrel::strel(asrm_mis, estimates = c("lambda6"), Bayes = FALSE, n.boot = 100, para.boot = TRUE)
   expect_equal(as.numeric(ee$freq$est$freq_lambda6), c(0.7927271),
                tolerance = tol)
   expect_equal(as.numeric(ee$freq$conf$low$freq_lambda6), 0.7188984, tolerance = tol)
@@ -102,7 +107,8 @@ test_that("Results with input cov matrix are correct", {
   data(asrm, package = "Bayesrel")
   cc <- cov(asrm)
   set.seed(1234)
-  ee <- Bayesrel::strel(cov.mat = cc, estimates = c("lambda2"), n.iter = 100, n.chains = 2, n.boot = 100, n.obs = 500)
+  ee <- Bayesrel::strel(cov.mat = cc, estimates = c("lambda2"), n.iter = 100, n.chains = 2, n.boot = 100, n.obs = 500,
+                        disableMCMCCheck = TRUE)
   expect_equal(as.numeric(ee$Bayes$cred$up$Bayes_lambda2), c(0.8215358),
                tolerance = tol)
   expect_equal(as.numeric(ee$freq$est$freq_lambda2), c(0.7960336),
@@ -129,7 +135,8 @@ test_that("Bayesian estimates lambda2 and omega are correct with adjusted priors
   data(asrm, package = "Bayesrel")
   set.seed(1234)
   ee <- Bayesrel::strel(asrm_mis, estimates = c("lambda2", "omega"), n.iter = 100, freq = FALSE, n.chains = 2,
-                        k0 = 1, df0 = 10, a0 = 6, b0 = 10, m0 = 1, missing = "listwise", item.dropped = TRUE)
+                        k0 = 1, df0 = 10, a0 = 6, b0 = 10, m0 = 1, missing = "listwise", item.dropped = TRUE,
+                        disableMCMCCheck = TRUE)
 
   expect_equal(ee$Bayes$est$Bayes_lambda2, 0.7892705, tolerance = tol)
   expect_equal(ee$Bayes$est$Bayes_omega, 0.6815307, tolerance = tol)
